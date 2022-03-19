45e07d18dace08b0de1ea33cb1411b2a5c4193cc
frank <wholery@163.com>
2022-03-19 16:19:33 +0800

    remove generated file

--- vue/src/store/generated/deaswang/nft/deaswang.nft.nmint/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Minter } from "./module/types/nmint/minter";
-import { Minterwl } from "./module/types/nmint/minterwl";
-import { Params } from "./module/types/nmint/params";
-export { Minter, Minterwl, Params };
-declare const _default;
-export default _default;
